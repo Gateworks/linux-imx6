3d9ceca908ad318bf9599561a8509aac34bf99ee
Tim Harvey <tharvey@gateworks.com>
2013-12-27 11:03:27 -0800

    imx6: ventana: add FXOS8700 support
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- arch/arm/mach-mx6/board-mx6q_gwventana.c
+++ arch/arm/mach-mx6/board-mx6q_gwventana.c
@@ -819,9 +819,6 @@ static struct i2c_board_info mxc_i2c1_board_info[] __initdata = {
 };
 
 
-/* position 0-7 (alignment?) */
-static int mma8451_position = 1;
-
 void mx6_csi0_io_init(void)
 {
 	if (cpu_is_mx6q()) {
@@ -953,14 +950,16 @@ static struct i2c_board_info mxc_i2c2_board_info[] __initdata = {
 };
 
 /* Accelerometer */
+static int accel_position = 0; /* physical orientation of part on PCB (0-7) */
 static struct i2c_board_info ventana_mma8451_i2cinfo = {
-	I2C_BOARD_INFO("mma8451", 0x1c),
+	I2C_BOARD_INFO("mma8x5x", 0x1c),
 	.irq = gpio_to_irq(MX6Q_VENTANA_ACCEL_IRQ),
-	.platform_data = (void *)&mma8451_position,
+	.platform_data = (void *)&accel_position,
 };
 static struct i2c_board_info ventana_fxos8700_i2cinfo = {
-	I2C_BOARD_INFO("fxos8700", 0x1c),
+	I2C_BOARD_INFO("mma8x5x", 0x1e),
 	.irq = gpio_to_irq(MX6Q_VENTANA_ACCEL_IRQ),
+	.platform_data = (void *)&accel_position,
 };
 
 /* Audio Codec */
@@ -1841,6 +1840,7 @@ static int __init ventana_model_setup(void)
 				gpio_direction_input(IMX_GPIO_NR(4,9));
 
 				/* Accelerometer */
+				accel_position = 0;
 				i2c_new_device(i2c_get_adapter(2), &ventana_mma8451_i2cinfo);
 
 			} /* end GW5400-A */
@@ -1903,6 +1903,7 @@ static int __init ventana_model_setup(void)
 				sprintf(ventana_fb_data[3].disp_dev, "off");
 
 				/* Accelerometer */
+				accel_position = 0;
 				i2c_new_device(i2c_get_adapter(2), &ventana_fxos8700_i2cinfo);
 
 				/* CAN bus stby */
@@ -2188,6 +2189,7 @@ static int __init ventana_model_setup(void)
 			}
 
 			/* Accelerometer */
+			accel_position = 3;
 			i2c_new_device(i2c_get_adapter(2), &ventana_fxos8700_i2cinfo);
 
 			/* CAN bus stby */
@@ -2359,6 +2361,7 @@ static int __init ventana_model_setup(void)
 			}
 
 			/* Accelerometer */
+			accel_position = 3;
 			i2c_new_device(i2c_get_adapter(2), &ventana_fxos8700_i2cinfo);
 
 			/* CAN bus stby */
