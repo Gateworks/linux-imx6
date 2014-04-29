a9f3c9f3e07b4723ed8a13f5fa6bfcd96227010a
Tim Harvey <tharvey@gateworks.com>
2014-04-28 17:25:14 -0700

    ventana: fix model detection for new GW54xx variants
    Several new GW54xx models have been released and need to be treated the same.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- arch/arm/mach-mx6/board-mx6q_gwventana.c
+++ arch/arm/mach-mx6/board-mx6q_gwventana.c
@@ -1880,9 +1880,7 @@ static int __init ventana_model_setup(void)
 
 			} /* end GW5400-A */
 
-			else if ( (strncmp(info->model, "GW5400", 6) == 0)
-		         || (strncmp(info->model, "GW5410", 6) == 0)
-			) {
+			else {
 				/* UARTs */
 				SETUP_PADS(gw54xx_uart_pads);
 				if (info->config_uart0)
