dc174305cc1358efc64799d0ac6365bb948db6bc
Tim Harvey <tharvey@gateworks.com>
2014-04-28 17:23:01 -0700

    ventana: make sure uart2_en is setup correctly
    Add uart2_en pinmux for GW54xx
    Add uart2_en export and pinmux for GW52xx
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- arch/arm/mach-mx6/board-mx6q_gwventana.c
+++ arch/arm/mach-mx6/board-mx6q_gwventana.c
@@ -2043,6 +2043,7 @@ static int __init ventana_model_setup(void)
 #endif
 
 			/* serial console rs232 driver enable */
+			SETUP_PAD(PAD_SD4_DAT3__GPIO_2_11);
 			gpio_request(IMX_GPIO_NR(2,11), "uart2_en");
 			gpio_export(IMX_GPIO_NR(2,11), 0);
 			gpio_direction_output(IMX_GPIO_NR(2,11), 0);
@@ -2321,6 +2322,12 @@ static int __init ventana_model_setup(void)
 
 			/* PCI Reset */
 			mx6_ventana_pcie_data.pcie_rst = IMX_GPIO_NR(1, 29);
+
+			/* serial console rs232 driver enable */
+			SETUP_PAD(PAD_SD4_DAT3__GPIO_2_11);
+			gpio_request(IMX_GPIO_NR(2,11), "uart2_en");
+			gpio_export(IMX_GPIO_NR(2,11), 0);
+			gpio_direction_output(IMX_GPIO_NR(2,11), 0);
 
 			/* USB OTG Select (Front-Panel vs J8) */
 			SETUP_PAD_CFG(PAD_GPIO_2__GPIO_1_2,
