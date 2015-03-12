a70727f3e5b27d7be1f250eb9686d8dd328db8c5
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-03-12 08:44:01 +0100

    power_supply: Add driver private data
    Allow drivers to store private data inside power_supply structure for
    later usage in power supply operations.
    
    Usage of driver private data is necessary to access driver's state
    container object from power supply calls (like get_property()) if struct
    'power_supply' is a stored there as a pointer, for example:
    
    struct some_driver_info {
    	struct i2c_client       *client;
    	struct power_supply     *power_supply;
    	...
    }
    
    In such case one cannot use container_of() and must store pointer to
    state container as private data.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Reviewed-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Reviewed-by: Sebastian Reichel <sre@kernel.org>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -204,6 +204,9 @@ struct power_supply {
 	/* For APM emulation, think legacy userspace. */
 	int use_for_apm;
 
+	/* Driver private data */
+	void *drv_data;
+
 	/* private */
 	struct device *dev;
 	struct work_struct changed_work;
@@ -274,6 +277,7 @@ extern int power_supply_register(struct device *parent,
 extern void power_supply_unregister(struct power_supply *psy);
 extern int power_supply_powers(struct power_supply *psy, struct device *dev);
 
+extern void *power_supply_get_drvdata(struct power_supply *psy);
 /* For APM emulation, think legacy userspace. */
 extern struct class *power_supply_class;
 
